0648e7f06eebafa43257f2290957afcb043ce45d
TkachLermontov <62636323+TkachLermontov@users.noreply.github.com>
2020-11-27 17:12:42 +0500

    Add files via upload
    "main.py" is the main file of the program, whole methods are wrtten there. "UI.py" is file with a whole interface.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import sys
-from random import randint
+from random import randint, choice
 
-from PyQt5.QtGui import QPainter, QBrush, QPen
-from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QBrush
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from UI import Ui_MainWindow
 
 
-class Window(QMainWindow):
+class Window(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(Window, self).__init__()
-        uic.loadUi("UI.ui", self)
+        super().__init__()
+        # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
+        # остальное без изменений
+        self.setupUi(self)
 
         self.do_paint = False
         self.btn.clicked.connect(self.paint)
@@ -18,7 +21,9 @@ class Window(QMainWindow):
     def paintEvent(self, event):
         if self.do_paint:
             painter = QPainter(self)
-            painter.setBrush(QBrush(Qt.yellow, Qt.SolidPattern))
+            color = [Qt.yellow, Qt.blue, Qt.red, Qt.green, Qt.black, Qt.white, Qt.darkYellow,
+                     Qt.darkMagenta, Qt.darkCyan]
+            painter.setBrush(QBrush(choice(color), Qt.SolidPattern))
             x = randint(10, 400)
             painter.drawEllipse(180, 70, x, x)
 
